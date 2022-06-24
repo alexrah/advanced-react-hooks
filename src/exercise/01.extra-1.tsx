ea6d83d8df5518feafd824452538ebda39775c06
alessandro stoppato <alexrah@gmail.com>
2022-06-24 12:33:00 +0200

    src/exercise/01.extra-1.tsx src/__tests__/01.extra-1.js

---
+++ src/exercise/01.extra-1.tsx
@@ -0,0 +1,34 @@
+// useReducer: simple Counter
+// http://localhost:3000/isolated/exercise/01.js
+
+import * as React from 'react'
+import logger from "./../helpers/logger";
+
+
+
+function countReducer(prevState:number,action:number){
+    const lg = new logger();
+    lg.v('prevState',prevState);
+    lg.s('action',action);
+    return prevState + action
+}
+    
+function Counter({initialCount = 0, step = 1}) {
+    // 🐨 replace React.useState with React.useReducer.
+    // 💰 React.useReducer(countReducer, initialCount)
+    // const [count, setCount] = React.useState(initialCount)
+    const [count, setCount] = React.useReducer<React.Reducer<number, number>>(countReducer,initialCount)
+
+    // 💰 you can write the countReducer function so you don't have to make any
+    // changes to the next two lines of code! Remember:
+    // The 1st argument is called "state" - the current value of count
+    // The 2nd argument is called "newState" - the value passed to setCount
+    const increment = () => setCount(step)
+    return <button onClick={increment}>{count}</button>
+}
+
+function App() {
+    return <Counter />
+}
+
+export default App
