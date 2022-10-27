d41cfbf395a9f1d6f4a0364348f01d9f2f4ecac6
alessandro stoppato <alexrah@gmail.com>
2022-10-27 14:50:51 +0200

    src/exercise/01.retake.extra-2.tsx

--- src/exercise/01.retake.extra-2.tsx
+++ src/exercise/01.retake.extra-2.tsx
@@ -9,20 +9,24 @@ function Counter({initialCount = 0, step = 1}) {
   // 💰 React.useReducer(countReducer, initialCount)
   // const [count, setCount] = React.useState(initialCount)
 
-  const countReducer = (prevState:number,newValue:number) => {
+  type tState = {
+    count: number
+  }
+
+  const countReducer = (prevState:tState,newValue:number) => {
     const lg = new logger();
     lg.v('prevState',prevState);
     lg.v('newValue',newValue);
-    return prevState + newValue;
+    return newValue;
   }
 
-  const [count, setCount] = React.useReducer(countReducer,initialCount)
+  const [count, setCount] = React.useReducer<tState>(countReducer,{ count: initialCount})
 
   // 💰 you can write the countReducer function so you don't have to make any
   // changes to the next two lines of code! Remember:
   // The 1st argument is called "state" - the current value of count
   // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(step)
+  const increment = () => setCount({count: count + step})
   return <button onClick={increment}>{count}</button>
 }
 
